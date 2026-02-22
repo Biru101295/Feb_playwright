e76b24f35128ca8f179bc92e43c7873b4b839c88
Birendra <v-bipandit@microsoft.com>
2026-02-22 14:06:30 +0530

    fixed report issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'playwright-reportss', open: 'never' }]],
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -32,7 +32,6 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    timeout: 30000,
   },
 
   /* Configure projects for major browsers */
